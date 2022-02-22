d299e621ae83536c32f6b359ad37745698fbb03b
Mykola_Hrytsenko <niksandler@gmail.com>
2022-02-23 00:44:52 +0200

    фикс для кнопки моб меню
    -добавил в скрипт еще одну строку для мобильной кнопки
    -дал название из этой строки кнопке
    =модалка запускаеться
    -пробовал играться с z-index, но мобильное меню всё равно ближе по Z, даже с меньшим индексом.

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,7 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
+      openModalBtn: document.querySelector('[data-modal-open-mob]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
     };
