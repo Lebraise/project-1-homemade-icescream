da07959490656cef485c2c267827822320fee3bb
Vladislav Gulak <careshynurary@gmail.com>
2022-02-23 10:54:11 +0200

    fix modal window open

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,6 +7,10 @@ btnclose.onclick = function () {
 };
 function orderBtn() {
   modal.style.display = 'block';
+  const mobModal = document.querySelector('.mob-menu');
+if (mobModal) {
+  mobModal.classList.add('is-hidden');
+}
 }
 btnhead.onclick = orderBtn;
 btnmob.onclick = orderBtn;
